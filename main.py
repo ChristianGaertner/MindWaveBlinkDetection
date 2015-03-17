228392abbdaf969b2f29949043fd50bdbc8c5f09
Christian Gärtner <chrisdagardner@gmail.com>
2015-03-17 22:18:06 +0100

    Improved read() function

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+'''
 
+Analyse raw mindwave curves.
 
+MIT License
+===========
+
+Copyright (c) 2015 Christian Gaertner <chrisdagardner@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+'''
 
 def read(file):
+	"""reads the data file into a list and converts them to integers"""
 	with open(file) as f:
-		content = f.readlines();
-		print content
+		# read all data into a list
+		strings = f.readlines()
+		# remove title from list
+		del strings[0]
+		# convert to integers, but remove empty strings first
+		return map(int, filter(None, strings))
 
 
 def main():
-	read('data/data0.txt')
+	print read('data/data0.txt')
 
 
 if __name__ == "__main__":
